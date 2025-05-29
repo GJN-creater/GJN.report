54a492d9b51ea5e9f705edd2a34b0a35e02941ff
나건주 <ngj@nplohs.com>
2025-05-29 20:45:07 +0900

    설명

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,17 +1,23 @@
-// src/lib/firebase.ts
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// Import the functions you need from the SDKs you need
 import { getStorage } from "firebase/storage";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "너의 키",
+  apiKey: "AIzaSyAoojohNyinBZKHC1xeplS-i1tbTpzxuPY",
   authDomain: "my-report-f4176.firebaseapp.com",
   projectId: "my-report-f4176",
-  storageBucket: "my-report-f4176.appspot.com",
-  messagingSenderId: "너의 값",
-  appId: "너의 값"
+  storageBucket: "my-report-f4176.firebasestorage.app",
+  messagingSenderId: "772097498731",
+  appId: "1:772097498731:web:3afce582a9a72510ce9b2e",
+  measurementId: "G-2Z29LTD4N7"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
 export const storage = getStorage(app);
